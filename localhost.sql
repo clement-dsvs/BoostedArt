9922098966c999bd803305c8ef592effbd71368a
Clement <dsvsclement@gmail.com>
2021-01-05 11:23:13 +0100

    Create localhost.sql

---
+++ localhost.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : mar. 05 jan. 2021 à 10:18
+-- Version du serveur :  5.7.24
+-- Version de PHP : 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `boosted-art`
+--
+CREATE DATABASE IF NOT EXISTS `boosted-art` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `boosted-art`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bundle`
+--
+
+CREATE TABLE `bundle` (
+  `id` int(11) NOT NULL,
+  `link` text NOT NULL,
+  `title` varchar(40) NOT NULL,
+  `description` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `publication` datetime NOT NULL,
+  `expiration` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaries`
+--
+
+CREATE TABLE `commentaries` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `rating` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_bundle` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `file`
+--
+
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL,
+  `id_bundle` int(11) NOT NULL,
+  `url` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `following`
+--
+
+CREATE TABLE `following` (
+  `id` int(11) NOT NULL,
+  `id_follower` int(11) NOT NULL,
+  `id_followed` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_bundle` int(11) NOT NULL,
+  `transaction_date` datetime NOT NULL,
+  `transaction_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `point_of_interest`
+--
+
+CREATE TABLE `point_of_interest` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_theme` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rank`
+--
+
+CREATE TABLE `rank` (
+  `id` int(11) NOT NULL,
+  `rank` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `rank`
+--
+
+INSERT INTO `rank` (`id`, `rank`) VALUES
+(1, 'user'),
+(2, 'artist');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `mail` varchar(40) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `bio` text,
+  `id_rank` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `bundle`
+--
+ALTER TABLE `bundle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commentaries`
+--
+ALTER TABLE `commentaries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `following`
+--
+ALTER TABLE `following`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `point_of_interest`
+--
+ALTER TABLE `point_of_interest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rank`
+--
+ALTER TABLE `rank`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_rank` (`id_rank`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `bundle`
+--
+ALTER TABLE `bundle`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commentaries`
+--
+ALTER TABLE `commentaries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `following`
+--
+ALTER TABLE `following`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `point_of_interest`
+--
+ALTER TABLE `point_of_interest`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `rank`
+--
+ALTER TABLE `rank`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_rank`) REFERENCES `rank` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
