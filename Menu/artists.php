786a16b436ea7d41baf05702d02932b319d496d6
Pyxah <73198262+Pyxah@users.noreply.github.com>
2021-01-06 14:06:26 +0100

    Update artists.php

--- Menu/artists.php
+++ Menu/artists.php
@@ -26,12 +26,12 @@ catch(Exception $e) {
 
 //La fonction de recherche est en cours de réparationnage (oui Jeremy c'est fait exprès ne fait pas une syncope plz)
 
-$artistes = $bdd->query('SELECT name FROM user ORDER BY id DESC');
+/* $artistes = $bdd->query('SELECT name FROM user ORDER BY id DESC');
 	if(isset($_GET['recherche']) AND !empty($_GET['recherche'])) {
 	   $recherche = htmlspecialchars($_GET['recherche']);
 	   $artistes = $bdd->query('SELECT name FROM user WHERE name LIKE "%'.$recherche.'%" ORDER BY id DESC');
 	   echo $artistes[1];
-	}
+	} */
 
 $getAllArtists = $bdd->query('SELECT * FROM user WHERE id_rank = 2 ORDER BY creation DESC');
 foreach($getAllArtists->fetchAll() as $data) {
