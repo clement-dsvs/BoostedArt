23d5879856abac91c7ff4ddab72fdb231394fd5c
Pyxah <73198262+Pyxah@users.noreply.github.com>
2021-01-06 13:36:01 +0100

    Update profile.php

--- Menu/profile.php
+++ Menu/profile.php
@@ -1,90 +1,45 @@
 <html>
 <?php
-    require_once 'header.php';
-		 
-	/*if(isset($_SESSION['id'])) {
-	   $requser = $bdd->prepare("SELECT * FROM user WHERE id = ?");
-	   $user = $requser->fetch();
-	   if(isset($_POST['newname']) AND !empty($_POST['newname']) AND $_POST['newname'] != $user['name']) {
-	        $newname = htmlspecialchars($_POST['newname']);
-	        $insertname = $bdd->prepare("UPDATE user SET name = ? WHERE id = ?");
-	        $insertname->execute(array($newname, $_SESSION['id']));
-	        header('Location: profile.php'.$_SESSION['id']);
-       }
-       
-	   if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['mail']) {
-	        $newmail = htmlspecialchars($_POST['newmail']);
-	        $insertmail = $bdd->prepare("UPDATE user SET mail = ? WHERE id = ?");
-	        $insertmail->execute(array($newmail, $_SESSION['id']));
-	        header('Location: profile.php'.$_SESSION['id']);
-	   }
-       
-	   if(isset($_POST['newphone']) AND !empty($_POST['newphone']) AND $_POST['newphone'] != $user['phone']) {
-	        $newphone = htmlspecialchars($_POST['newphone']);
-	        $insertphone = $bdd->prepare("UPDATE user SET phone = ? WHERE id = ?");
-	        $insertphone->execute(array($newphone, $_SESSION['id']));
-	        header('Location: profile.php'.$_SESSION['id']);
-       }
-       
-       	   if(isset($_POST['newbio']) AND !empty($_POST['newbio']) AND $_POST['newbio'] != $user['bio']) {
-	        $newbio = htmlspecialchars($_POST['newbio']);
-	        $insertbio = $bdd->prepare("UPDATE user SET bio = ? WHERE id = ?");
-	        $insertbio->execute(array($newbio, $_SESSION['id']));
-	        header('Location: profile.php'.$_SESSION['id']);
-	   }
+    require_once 'header.php'; ?>
+    <head>
+        <title>Mon profil</title>
+    </head>
 
-       if(isset($_POST['newmdp']) AND !empty($_POST['newmdp'])) {
-            $mdp = sha1($_POST['newmdp']); {
-	        $insertmdp = $bdd->prepare("UPDATE user SET password = ? WHERE id = ?");
-	        $insertmdp->execute(array($mdp, $_SESSION['id']));
-	        header('Location: profile.php'.$_SESSION['id']);
-       } */
-    ?> 
-    
-	<html>
-	   <head>
-	      <title>EDIT PROFIL</title>
-	   </head>
-	   <body>
-            <div>
-                <?php var_dump($user) ?>
-	            <h2>Profil de <?php echo $user['name']; ?></h2>
-	            <br><br>
-	            Nom : <?php echo $user['name']; ?>
-	            <br>
-	            Mail : <?php echo $user['mail']; ?>
-	            <br>
-                Téléphone : <?php echo $user['phone']; ?>
-                <br>
-                Bio : <?php echo $user['bio']; ?>
-                <br>
-	        </div>
+    <body>
 
-	        <div>
-	            <h2>Edition de mon profil</h2>
-	                <div>
-                    <form method="POST" action="includes/updateprofile.php" enctype="multipart/form-data">
-	                    <label>Nom :</label>
-	                    <input type="text" name="newname" placeholder="Nom" value="<?php echo $user['name']; ?>"/><br><br>
-	                    <label>Mail :</label>
-                        <input type="text" name="newmail" placeholder="Mail" value="<?php echo $user['mail']; ?>" /><br><br>
-                        <label>Téléphone :</label>
-                        <input type="text" name="newphone" placeholder="Téléphone" value="<?php echo $user['phone']; ?>" /><br><br>
-                        <label>Bio :</label>
-	                    <textarea type="text" name="newbio" placeholder="Bio"><?php echo $user['bio']; ?></textarea><br><br>
-	                    <label>Mot de passe :</label>
-                        <input type="password" name="newmdp" placeholder="Mot de passe"/><br><br>
-	                    <input type="submit" value="Mettre à jour mon profil" />
-                        </form>
-	                </div>
-	        </div>
-	   </body>
-    </html>
+    <?php
 
-    <?php   
-    /*   }
-        else {
-	        header("Location: login.php");
-            } 
-        } */   
+try {
+    $bdd=new PDO('mysql:host=localhost;dbname=boosted-art;charset=UTF8', 'root','root');
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    }
+
+    catch(Exception $e) {
+    die('Erreur :' .$e->getMessage());
+    }
+
+/* S'il n'y a pas de session alors on ne va pas sur cette page (mais ça marche pô encore lolilol)
+        if(!isset($_SESSION['id'])){ 
+        header('Location: index.php'); 
+        exit; 
+        }
+*/
+
+        $afficher_profil = $bdd->query("SELECT * FROM user");
+        $afficher_profil = $afficher_profil->fetch(); 
+
     ?>
+
+        <h2>Voici le profil de <?= $afficher_profil['name'] ?></h2>
+        <div>Quelques informations sur vous : </div>
+
+        <ul>
+            <li>Votre mail est : <?= $afficher_profil['mail'] ?></li>
+            <li>Votre numéro de téléphone est : <?= $afficher_profil['phone'] ?></li>
+            <li>Votre bio est : <?= $afficher_profil['bio'] ?></li>
+            <li>Votre compte a été crée le : <?= $afficher_profil['creation'] ?></li>
+        </ul>
+        <button id="edit"><a href="updateprofile.php">Editer le profil</a></button>
+
+    </body>
+</html>
