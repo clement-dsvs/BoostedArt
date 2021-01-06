30dfa45b6d9d4905ff9195b125d352043837caed
Clement <dsvsclement@gmail.com>
2021-01-06 14:47:00 +0100

    1

--- Menu/shop.php
+++ Menu/shop.php
@@ -14,18 +14,17 @@
                                 <img src='bundle/{$bundle[0]}/cover.jpg' alt='' class='vitrine'>
                                 <a class='title'>{$bundle[2]}</p>
                                 <p class='artiste'>{$bundle[4]}</p>
-
-                                <div class='buy-container'>
-                                        <button class='buy'><i class='fas fa-shopping-cart'></i></button>
-                                        <section class='quantity'>
-                                        <button class='minus' onclick='minus()'>-</button>
-                                        <input type='text' class='number' value=1>
-                                        <button class='plus'>+</button>
-                                         </section>
-                                </div>
+                                <form target='paypal' action='https://www.paypal.com/cgi-bin/webscr' method='post'>
+                                        <input type='hidden' name='cmd' value='_s-xclick'>
+                                        <input type='hidden' name='hosted_button_id' value='ZTYEUWVB4AUXN'>
+                                        <input type='image' src='https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_cart_SM.gif' border='0' name='submit' alt='PayPal, le réflexe sécurité pour payer en ligne'>
+                                        <img alt='' border='0' src='https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif' width='1' height='1'>
+                                </form>
                         </div>
                 ");
         }
+        //<button class='buy'><i class='fas fa-shopping-cart'></i></button>ZTYEUWVB4AUXN
+
 ?>
 
 </html>
