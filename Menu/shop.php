c5c0f774614344b09814168183c56106892be5c2
Clement <dsvsclement@gmail.com>
2021-01-06 14:55:41 +0100

    Change Style Bundle Shop

--- Menu/shop.php
+++ Menu/shop.php
@@ -9,18 +9,14 @@
 
         foreach($bundles as $bundle){
                 echo("
-                        <div class='item' id='item-{$bundle[0]}'>
+                <div class='item' id='item-{$bundle[0]}'>
 
-                                <img src='bundle/{$bundle[0]}/cover.jpg' alt='' class='vitrine'>
-                                <a class='title'>{$bundle[2]}</p>
-                                <p class='artiste'>{$bundle[4]}</p>
-                                <form target='paypal' action='https://www.paypal.com/cgi-bin/webscr' method='post'>
-                                        <input type='hidden' name='cmd' value='_s-xclick'>
-                                        <input type='hidden' name='hosted_button_id' value='ZTYEUWVB4AUXN'>
-                                        <input type='image' src='https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_cart_SM.gif' border='0' name='submit' alt='PayPal, le réflexe sécurité pour payer en ligne'>
-                                        <img alt='' border='0' src='https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif' width='1' height='1'>
-                                </form>
-                        </div>
+                <img src='bundle/{$bundle[0]}/cover.jpg' alt='' class='Minia'>
+                <p class='title'>{$bundle[2]}</p>
+                <p class='Artist'>{$bundle[3]}</p>
+                <p class='Desc'>{$bundle[4]}</p>
+                        
+            </div>
                 ");
         }
         //<button class='buy'><i class='fas fa-shopping-cart'></i></button>ZTYEUWVB4AUXN
