9e45a34a45875987e9b59d954f7c7f97923457f0
Pyxah <73198262+Pyxah@users.noreply.github.com>
2021-01-06 13:51:21 +0100

    Update artists.php

--- Menu/artists.php
+++ Menu/artists.php
@@ -24,11 +24,13 @@ catch(Exception $e) {
     die('Erreur :' .$e->getMessage());
 }
 
+//La fonction de recherche est en cours de réparationnage (oui Jeremy c'est fait exprès ne fait pas une syncope plz)
+
 $artistes = $bdd->query('SELECT name FROM user ORDER BY id DESC');
 	if(isset($_GET['recherche']) AND !empty($_GET['recherche'])) {
 	   $recherche = htmlspecialchars($_GET['recherche']);
 	   $artistes = $bdd->query('SELECT name FROM user WHERE name LIKE "%'.$recherche.'%" ORDER BY id DESC');
-	   echo $artistes;
+	   echo $artistes[1];
 	}
 
 $getAllArtists = $bdd->query('SELECT * FROM user WHERE id_rank = 2 ORDER BY creation DESC');
@@ -37,10 +39,12 @@ foreach($getAllArtists->fetchAll() as $data) {
 
 <div class="a">
     <div class="nd">
-        <div id="name">Nom de l'artiste : <?php echo $data['name']; ?><br></div>
-        <div id="date">Membre depuis : <?php echo $data['creation']; ?><br></div>
+        <div id="name"><b>• Nom de l'artiste : </b><?php echo $data['name']; ?><br><br></div>
+        <div id="bio">"<i> <?php echo $data['bio']; ?> </i>"<br><br>
+        <div id="date">Membre depuis : <?php echo $data['creation']; ?><br><br></div>
     </div>
-    <button id="consult">Consulter la page</>
+    <button id="consult">Consulter la page</button>
+    <br><br><br>
 </div>
 
 <?php 
