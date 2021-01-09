598495b4a04cf87604bf14999b7c5df5ade47fbb
Kharnaim/Aghilas <73821494+Kharnaim@users.noreply.github.com>
2021-01-09 15:42:59 +0100

    Update profile.php

--- Menu/profile.php
+++ Menu/profile.php
@@ -12,7 +12,7 @@
     <?php
 
 try {
-    $bdd=new PDO('mysql:host=localhost;dbname=boosted-art;charset=UTF8', 'root','root');
+    $bdd=new PDO('mysql:host=localhost;dbname=boosted-art;charset=UTF8', 'root','');
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     }
 
@@ -25,28 +25,45 @@ try {
 
     ?>
 
-        <h2>Voici le profil de <?= $afficher_profil['name'] ?></h2>
-        <div>Quelques informations sur vous : </div>
-
-        <ul>
-            <li>Votre mail est : <?= $afficher_profil['mail'] ?></li>
-            <li>Votre numéro de téléphone est : <?= $afficher_profil['phone'] ?></li>
-            <li>Votre bio est : <?= $afficher_profil['bio'] ?></li>
-            <li>Votre compte a été crée le : <?= $afficher_profil['creation'] ?></li>
+<ul>
+        <div class="banner">
+            <div class="infophoto">
+                <div class="info">
+                    <h2><?= $afficher_profil['name'] ?></h2>
+                     <div>Quelques informations sur vous :
+                    <ul>
+                     <li>Votre mail est : <?= $afficher_profil['mail'] ?></li>
+                    <li>Votre numéro de téléphone est : <?= $afficher_profil['phone'] ?></li>
+                    </ul>
+                    <button id="edit"><a href="updateprofile.php">Editer le profil</a></button>
+                    <?php
+                    if($afficher_profil["id_rank"] != 2){
+                    echo("<form action='includes/becomeArtist.inc.php' method='post'>
+                    <input type='hidden' name='user_id' value='{$_SESSION['user-id']}'>
+                    <button type='submit' name='submit'>Devenir Un artiste !</button>   
+                    </form>");
+                    }
+                    ?>
+            </div>
+            <div class="photo">
+                <img src="" alt="Ma photo"/>
+            </div>
+        </div>
+    </ul>
+    <ul> 
+        <div class="section">
+            <div class="bio">
+                Votre bio est : <?= $afficher_profil['bio'] ?>
+            </div>
+        </div>
+    </ul>
+    <ul> 
+        <div class="section2">
+            <div class="infos">
+            Votre compte a été crée le : <?= $afficher_profil['creation'] ?>
             <li>Il s'agit d'un : <?php if ($afficher_profil['id_rank'] == 2){ echo"Artiste";} else {echo"utilisateur";} ?></li>
-        </ul>
-        <button id="edit"><a href="updateprofile.php">Editer le profil</a></button>
+            </div>
+        </div>
+    </ul>
 
-    <?php
-        if($afficher_profil["id_rank"] != 2){
-            echo("<form action='includes/becomeArtist.inc.php' method='post'>
-        <input type='hidden' name='user_id' value='{$_SESSION['user-id']}'>
-        <button type='submit' name='submit'>Devenir Un artiste !</button>   
-    </form>");
-        }
-    ?>
-
-   
-
-    </body>
-</html>
+</body>
