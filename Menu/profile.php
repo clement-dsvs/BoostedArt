21c441e490ef02fd8c82c5f7473c0c472d4ea507
Kharnaim/Aghilas <73821494+Kharnaim@users.noreply.github.com>
2021-01-09 16:11:54 +0100

    Update profile.php

--- Menu/profile.php
+++ Menu/profile.php
@@ -7,7 +7,7 @@
     }
     
 ?>
-    <title>BVotre Profil</title>
+    <title>Votre Profil</title>
     <link rel="stylesheet" href="style/style-profile.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript" src="script.js"></script>
